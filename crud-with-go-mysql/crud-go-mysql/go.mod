e40de68f4d6f06f83440a411d42f957d75a16233
wx2sim <71595632+wx2sim@users.noreply.github.com>
2021-11-15 20:55:15 +0100

    adding jwt authentification to my service

--- crud-with-go-mysql/crud-go-mysql/go.mod
+++ crud-with-go-mysql/crud-go-mysql/go.mod
@@ -2,4 +2,7 @@ module crud-go-mysql
 
 go 1.17
 
-require github.com/go-sql-driver/mysql v1.6.0 // indirect
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+)
--- crud-with-go-mysql/crud-go-mysql/main.go
+++ crud-with-go-mysql/crud-go-mysql/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"text/template"
 
+	jwt "github.com/dgrijalva/jwt-go"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -13,6 +15,34 @@ type Employee struct {
 	Id   int
 	Name string
 	City string
+}
+
+var mySigningKey = []byte("benchmatrix")
+
+func isAuthorized(endpoint func(http.ResponseWriter, *http.Request)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Header["Token"] != nil {
+
+			token, err := jwt.Parse(r.Header["Token"][0], func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("There was an error")
+				}
+				return mySigningKey, nil
+			})
+
+			if err != nil {
+				fmt.Fprintf(w, err.Error())
+			}
+
+			if token.Valid {
+				endpoint(w, r)
+			}
+		} else {
+
+			fmt.Fprintf(w, "Not Authorized")
+		}
+	})
 }
 
 func dbConn() (db *sql.DB) {
@@ -149,14 +179,19 @@ func Delete(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", 301)
 }
 
-func main() {
+func HandelRequest() {
 	log.Println("Server started on: http://localhost:8080")
-	http.HandleFunc("/", Index)
-	http.HandleFunc("/show", Show)
-	http.HandleFunc("/new", New)
-	http.HandleFunc("/edit", Edit)
-	http.HandleFunc("/insert", Insert)
-	http.HandleFunc("/update", Update)
-	http.HandleFunc("/delete", Delete)
-	http.ListenAndServe(":8080", nil)
+	http.Handle("/", isAuthorized(Index))
+	http.Handle("/show", isAuthorized(Show))
+	http.Handle("/new", isAuthorized(New))
+	http.Handle("/edit", isAuthorized(Edit))
+	http.Handle("/insert", isAuthorized(Insert))
+	http.Handle("/update", isAuthorized(Update))
+	http.Handle("/delete", isAuthorized(Delete))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func main() {
+
+	HandelRequest()
 }
